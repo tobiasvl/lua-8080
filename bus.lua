03b6a3c8a15b5783eeab306db4cefa07a60e34b3
Tobias V. Langhoff <tobias@langhoff.no>
2020-07-03 22:13:37 +0200

    Optimize bus (closes #6) and stub out memory breakpoint
    Memory breakpoint (and uninitialized RAM) should go into a "Debug" object or something later

--- bus.lua
+++ bus.lua
@@ -2,44 +2,41 @@ local bit = bit or require('bit32')
 
 local bus = setmetatable({
     memory_map = {},
-    breakpoint = {
-        address = nil,
-        read = false,
-        write = false
-    },
+    --breakpoint = {
+    --    address = nil,
+    --    read = false,
+    --    write = false
+    --},
     connect = function(self, startAddress, module)
-        self.memory_map[{
-            startAddress,
-            module.size
-        }] = module
+        local tbl = { startAddress = startAddress, module = module }
+        for i = startAddress, startAddress + module.size do
+            self.memory_map[i] = tbl
+        end
     end
 }, {
     __index = function(self, address)
         address = bit.band(address, 0xFFFF)
-        if address == self.breakpoint.address and self.breakpoint.read then
-            self.cpu.pause = true
-        end
-        for addresses, module in pairs(self.memory_map) do
-            if address >= addresses[1] and address < addresses[1] + addresses[2] then
-                return module[address - addresses[1]]
-            end
+        --if address == self.breakpoint.address and self.breakpoint.read then
+        --    self.cpu.pause = true
+        --end
+        local tbl = self.memory_map[address]
+        if tbl then
+            return tbl.module[address - tbl.startAddress]
         end
         print("Attempted read at unmapped address " .. string.format("%04X", address))
         return 0
     end,
     __newindex = function(self, address, newValue)
-        if address == self.breakpoint.address and self.breakpoint.write then
-            self.cpu.pause = true
-        end
-        for addresses, module in pairs(self.memory_map) do
-            if address >= addresses[1] and address < addresses[1] + addresses[2] then
-                module[address - addresses[1]] = newValue
-                return
-            end
+        --if address == self.breakpoint.address and self.breakpoint.write then
+        --    self.cpu.pause = true
+        --end
+        local tbl = self.memory_map[address]
+        if tbl then
+            tbl.module[address - tbl.startAddress] = newValue
+            return
         end
         print("Attempted write at unmapped address " .. string.format("%04X", address) .. " with value " .. newValue)
-    end,
-    __len = function(self) return self.eprom.startAddress + self.eprom.size end -- LuaJIT doesn't support this? :(
+    end
 })
 
 return bus
