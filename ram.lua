f209522b84c299926563486345a60929ba036c39
Tobias V. Langhoff <tobias@langhoff.no>
2020-07-03 12:41:28 +0200

    Stub uninitialized, random RAM for optimization
    It's a nice feature but I'll have to figure out a better way to do it later

--- ram.lua
+++ ram.lua
@@ -1,26 +1,30 @@
 return function(length, initial_value)
     local self = {
         size = length,
-        memory = {},
-        initialized = setmetatable({}, {
-            __index = function() return false end
-        }),
-        set_uninitialized_value = function(self, initial_value)
-            self.uninitialized_random = not initial_value
-            for i = 0, length - 1 do
-                if not self.initialized[i] then
-                    self.memory[i] = initial_value or math.random(0, 255)
-                end
-            end
-        end
+        --memory = {},
+        --initialized = setmetatable({}, {
+        --    __index = function() return false end
+        --}),
+        --set_uninitialized_value = function(self, initial_value)
+        --    self.uninitialized_random = not initial_value
+        --    for i = 0, length - 1 do
+        --        if not self.initialized[i] then
+        --            self.memory[i] = initial_value or math.random(0, 255)
+        --        end
+        --    end
+        --end
     }
-    self:set_uninitialized_value(initial_value)
-    return setmetatable(self, {
-        __index = self.memory,
-        __newindex = function(self, address, value)
-            self.memory[address] = value
-            drawflag = true
-            self.initialized[address] = true
-        end
-    })
+    --self:set_uninitialized_value(initial_value)
+    for i = 0, length - 1 do
+        self[i] = 0
+    end
+    --return setmetatable(self, {
+    --    __index = self.memory,
+    --    __newindex = function(self, address, value)
+    --        self.memory[address] = value
+    --        drawflag = true
+    --        --self.initialized[address] = true
+    --    end
+    --})
+    return self
 end
